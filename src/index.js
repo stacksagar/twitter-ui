de4800d06e43e7647d98c621f932118749594d91
stacksagar <secure.js.info@gmail.com>
2021-03-07 20:34:58 +0600

    updated

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import App from './App';
 import { BrowserRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
-  <Router basename={process.env.PUBLIC_URL}>
+  // <Router basename={process.env.PUBLIC_URL}>
+  <React.StrictMode> 
     < App />
-  </Router>
+  </React.StrictMode>
+  // </Router>
   , document.getElementById('root')
 );
